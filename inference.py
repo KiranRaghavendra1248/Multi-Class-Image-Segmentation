b560f093d126907a0ff21e6f778617268b19c87e
KiranRaghavendra1248 <kiran.raghu.1248@gmil.com>
2024-04-01 09:30:50 -0700

    Inference script shows original & segmented image

--- inference.py
+++ inference.py
@@ -46,45 +46,49 @@ transform = transforms.Compose([
      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# # Infinite Face Detection Loop
-# v_cap = cv2.VideoCapture(0)
-# v_cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
-# v_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
-# # Check if the webcam is opened successfully
-# if not v_cap.isOpened():
-#     print("Error: Could not open webcam.")
-#     exit()
-# start = time.time()
-# prev = 0
-# while(True):
-#     time_elapsed = time.time()-prev
-#     ret,frame = v_cap.read()
-#     # Preprocess the frame
-#     input_tensor = transform(frame).unsqueeze(0)
-#     # Perform inference
-#     with torch.no_grad():
-#         output = model(input_tensor)
-#     segmented_image = create_image_from_output(output)
-#     segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
-#     # Concatenate the original frame and segmented image horizontally
-#     # Display the combined frame in a window
-#     cv2.imshow('Segmented',segmented_image)
-#
-#     if cv2.waitKey(delay) & 0xFF==ord('q'):
-#         break
-#
-# v_cap.release()
-# cv2.destroyAllWindows()
+# Infinite Face Detection Loop
+v_cap = cv2.VideoCapture(0)
+v_cap.set(cv2.CAP_PROP_FRAME_WIDTH, 256)
+v_cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)
+# Check if the webcam is opened successfully
+if not v_cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+start = time.time()
+prev = 0
+while(True):
+    time_elapsed = time.time()-prev
+    ret,frame = v_cap.read()
+    # Preprocess the frame
+    if frame.any():
+        input_tensor = transform(frame).unsqueeze(0)
+        # Perform inference
+        with torch.no_grad():
+            output = model(input_tensor)
+        segmented_image = create_image_from_output(output)
+        segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
+        segmented_image = cv2.resize(segmented_image,(frame.shape[1],frame.shape[0]))
+        # Concatenate the original frame and segmented image horizontally
+        # Display the combined frame in a window
+        combined_frame = cv2.hconcat([frame, segmented_image])
+        # Display the combined frame in a window
+        cv2.imshow('Original vs Segmented',combined_frame)
 
-# Process single image
-img_file = "trial_images/img3.jpg"
-img = cv2.imread(img_file)
-input_tensor = transform(img).unsqueeze(0)
-with torch.no_grad():
-    output = model(input_tensor)
-segmented_image = create_image_from_output(output)
-segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
-cv2.imwrite("results/img3.png",segmented_image)
+        if cv2.waitKey(delay) & 0xFF==ord('q'):
+            break
+
+v_cap.release()
+cv2.destroyAllWindows()
+
+# # Process single image
+# img_file = "trial_images/img3.jpg"
+# img = cv2.imread(img_file)
+# input_tensor = transform(img).unsqueeze(0)
+# with torch.no_grad():
+#     output = model(input_tensor)
+# segmented_image = create_image_from_output(output)
+# segmented_image = cv2.cvtColor(segmented_image, cv2.COLOR_RGB2BGR)
+# cv2.imwrite("results/img3.png",segmented_image)
 
 
 
